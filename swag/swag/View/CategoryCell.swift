727b529a12468fad4e620126196e7cd43ddf7963
Jesse Hill <jsnahill@gmail.com>
2017-09-30 15:18:29 -0500

    Add table view with custom cell view and controller

---
+++ swag/swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  swag
+//
+//  Created by Jesse Hill on 9/30/17.
+//  Copyright © 2017 Jesse Hill. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
