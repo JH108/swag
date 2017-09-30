014f7a4069ccca2d80db70e3faf7630c59022a67
Jesse Hill <jsnahill@gmail.com>
2017-09-30 17:06:12 -0500

    Create custom collection view and collection view cell

---
+++ swag/swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  swag
+//
+//  Created by Jesse Hill on 9/30/17.
+//  Copyright © 2017 Jesse Hill. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
