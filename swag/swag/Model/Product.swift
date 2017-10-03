9c44fe49ca70644fdeba3113cc5168351236b3ae
Jesse Hill <jsnahill@gmail.com>
2017-10-02 21:25:24 -0500

    Finish building out data service

---
+++ swag/swag/Model/Product.swift
@@ -0,0 +1,21 @@
+//
+//  Product.swift
+//  swag
+//
+//  Created by Jesse Hill on 10/2/17.
+//  Copyright © 2017 Jesse Hill. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
